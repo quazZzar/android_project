dd8fa61743538bd359d81576f43c7d104b9d74ac
quazzzar <abashuiasa@gmail.com>
2017-05-01 11:24:05 +0300

    99.1% Done.

--- app/src/main/java/com/example/jacob/findmypharmacy/MainActivity.java
+++ app/src/main/java/com/example/jacob/findmypharmacy/MainActivity.java
@@ -204,7 +204,7 @@ public class MainActivity extends Activity implements OnMapReadyCallback, Google
             }
         });
 
-     }
+    }
 
     protected void onStart() {
         mGoogleApiClient.connect();
